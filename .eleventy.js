e443d797d02eb5107015ccfe4ddd5994e4a795d8
Edwin Reyes <reyesedwin2022@gmail.com>
2022-03-19 19:51:41 -0400

    Github workflow

--- .eleventy.js
+++ .eleventy.js
@@ -119,7 +119,7 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "/",
+    pathPrefix: "/word-guess-game-doc",
     // -----------------------------------------------------------------
 
     // These are all optional (defaults are shown):
